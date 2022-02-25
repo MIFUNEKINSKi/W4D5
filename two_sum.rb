74e138fec4db0456352665aeb514631905d5336d
MifuneKinski <appacademystudent@Apps-Mac-mini-2.local>
2022-02-25 15:39:01 -0500

    finished two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -8,6 +8,41 @@ def brutish_two_sum(array,target)
 false
 end 
 
-arr = [0, 1, 5, 7]
+
 # p brutish_two_sum(arr, 10)
 
+def okay_two_sum(array, target)
+  sorted = array.sort
+  left = 0
+  right = sorted.length - 1 
+
+  while left < right 
+    sum = sorted[left] + sorted[right]
+    if sum == target 
+      return true 
+    elsif sum < target 
+      left += 1
+    else
+      right -= 1
+    end
+  end
+  return false
+end
+
+# p okay_two_sum(arr, 10)
+
+arr = [0, 1, 5, 7, 8, 5]
+def two_sum?(array, target)
+  hash = {}
+  array.each_with_index do |ele,idx|
+    difference = target - ele 
+    if hash.has_key?(difference)
+      return true 
+    else
+      hash[ele] = idx
+    end
+  end
+  return false
+end
+
+p two_sum?(arr, 17)
